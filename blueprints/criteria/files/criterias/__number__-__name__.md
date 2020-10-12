44e6da567d394ce9f40f2eb6c8c945649110bc7c
Melanie Sumner <melaniersumner@gmail.com>
2020-10-12 10:48:30 -0500

    added a criteria generator and tested it by generating a new criteria

---
+++ blueprints/criteria/files/criterias/__number__-__name__.md
@@ -0,0 +1,11 @@
+---
+title: <% criteriaTitle %>
+tags: <% criteriaTags %>
+# exists, couldExist, cannotExist
+lintingExists: <% lintingExists %> 
+testingExists: <% testingExists %>
+devMustAuthor: <% devMustAuthor %>
+manualTest: <% manualTest %>
+---
+
+Description goes here.
---
+++ blueprints/criteria/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { readdirSync } = require('fs');
+const { join } = require('path');
+
+module.exports = {
+  description: 'a blueprint to generate a new criteria markdown file',
+
+  locals(options) {
+    // Return custom template variables here.
+    return {
+      criteriaTitle: options.title,
+      criteriaTags: options.tags,
+      lintingExists: options.lintingExists,
+      testingExists: options.testingExists,
+      devMustAuthor: options.devMustAuthor,
+      manualTest: options.manualTest
+
+    };
+  },
+
+  fileMapTokens(options) {
+    // Return custom tokens to be replaced in your files
+    return {
+      __number__(options) {
+        let existingFiles = readdirSync(join(process.cwd(), 'criterias'));
+
+        let numbers = existingFiles.map((filename) => {
+          return parseInt(filename.split('-')[0])
+        });
+
+        let highestNumber = Math.max(...numbers);
+
+        // Logic to determine value goes here
+        return (highestNumber + 1).toString().padStart(2, '0');
+      }
+    };
+  },
+
+  // afterInstall(options) {
+    // Perform extra work here.
+  // }
+};
