152e3ed2b04c5e3fd245351fcbec272c37676b27
Melanie Sumner <melaniersumner@gmail.com>
2020-10-13 16:14:29 -0500

    reverting app for troubleshooting

---
+++ app/adapters/application.js
@@ -0,0 +1,13 @@
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
+
+export default class ApplicationAdapter extends JSONAPIAdapter {
+  urlForFindAll(modelName) {
+    const path = this.pathForType(modelName);
+    return `/${path}/all.json`;
+  };
+
+  urlForFindRecord(id, modelName) {
+    const path = this.pathForType(modelName);
+    return `/${path}/${id}.json`;
+  };
+}
--- app/router.js
+++ app/router.js
@@ -7,4 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+  this.route('details', function() {
+    this.route('detail');
+  });
 });
