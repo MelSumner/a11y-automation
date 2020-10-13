288a1ad8489347ebf7730ef9a90ae276c49ff5ee
Melanie Sumner <melaniersumner@gmail.com>
2020-10-13 16:32:11 -0500

    updating detail route to be dynamic

--- app/router.js
+++ app/router.js
@@ -8,6 +8,6 @@ export default class Router extends EmberRouter {
 
 Router.map(function() {
   this.route('details', function() {
-    this.route('detail');
+    this.route('detail', {path: '/detail/:detail_id'});
   });
 });
