1e968489513a3abb3b4d90477046c0acca0490f0
Melanie Sumner <melaniersumner@gmail.com>
2020-10-15 12:19:33 -0500

    added index route and prose about the app

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
-
+import { inject as service } from '@ember/service';
 export default class ApplicationRoute extends Route {
+  @service store;
+  model() {
+    return this.store.findAll('details'); 
+  }
 }
