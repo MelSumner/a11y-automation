6c53fc29118446d8ec17c389a96d2b9d0fa082a3
Melanie Sumner <melaniersumner@gmail.com>
2020-10-23 17:23:35 -0500

    added a details/index page with the data in tabular format

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service store;
+  model() {
+    return this.store.findAll('detail'); 
+  }
 }
