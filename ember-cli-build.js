2749e3132f5969a925bd6069ea458f9cd2a61eeb
Chris Manson <chris@manson.ie>
2020-11-03 17:24:41 +0000

    add index to prember

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    prember: {
+      urls: [
+        '/',
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
