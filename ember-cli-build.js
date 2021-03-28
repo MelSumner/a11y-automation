a15ee7e49aac0c59f8e13f882774b153b7ad8b0a
Melanie Sumner <melaniersumner@gmail.com>
2021-03-28 12:12:33 -0500

    refactor to use Tailwind

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,20 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const isProduction = EmberApp.env() === 'production';
+
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      './app/index.html',
+      './app/templates/**/*.hbs',
+      './app/components/**/*.hbs'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+}
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
@@ -8,6 +22,20 @@ module.exports = function(defaults) {
       urls: [
         '/',
       ]
+    },
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules']
+            }
+          },
+          require('tailwindcss')('./app/tailwind.config.js'),
+          ...isProduction ? [purgeCSS] : []
+        ]
+      }
     }
   });
 
