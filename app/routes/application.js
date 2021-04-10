94e0bf052793cb62ab3b6877a5c079638f2d854a
Chris Manson <chris@manson.ie>
2021-04-10 14:44:14 +0100

    remove unnecessary routes

--- app/routes/application.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-export default class ApplicationRoute extends Route {
-  @service store;
-  model() {
-    return this.store.findAll('violation');
-  }
-}
