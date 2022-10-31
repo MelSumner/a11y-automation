2accd5c985c7c8bd4eb088d51b7a623d5df9a458
Jose Bolos <josebolos@users.noreply.github.com>
2022-10-31 17:41:56 +0000

    Correct the name of Pa11y (Pally > Pa11y)

--- app/routes/automated-tools.js
+++ app/routes/automated-tools.js
@@ -63,7 +63,7 @@ export default class AutomatedToolsRoute extends Route {
         description: 'Custom Jest matcher for axe for testing accessibility.',
         link: 'https://github.com/nickcolley/jest-axe#readme'
       }, {
-        title: 'Pally',
+        title: 'Pa11y',
         category: 'Testing Tool',
         description: 'A command-line tool for testing the accessibility of any URL.',
         link: 'https://pa11y.org/'
