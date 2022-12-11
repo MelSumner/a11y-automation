c5b7f334af8d57e74592b0b7e871533d08435564
Shivareddy-Aluri <shiva.aluri@freshworks.com>
2022-12-11 18:37:15 +0530

    Dark by default

--- app/components/aa-navbar.js
+++ app/components/aa-navbar.js
@@ -3,14 +3,14 @@ import { action } from "@ember/object";
 import { tracked } from '@glimmer/tracking';
 
 export default class aaNavBarComponent extends Component {
-    @tracked selectedTheme = (document.querySelector('html').classList.value === 'dark');
+    @tracked isDarkTheme = true;
 
     toggleDarkTheme(match) {
-        this.selectedTheme = match;
+        this.isDarkTheme = match;
         document.querySelector('html').classList.toggle('dark', match);
     }
     @action
-    onToggle() {
+    onChangeTheme() {
         if (localStorage.theme === 'dark') {
             localStorage.theme = 'light';
             this.toggleDarkTheme(false)
