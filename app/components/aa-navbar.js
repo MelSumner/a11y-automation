aaaad4c5c430aefe70d4e776ed461bba583010e7
Shivareddy-Aluri <shiva.aluri@freshworks.com>
2022-12-11 13:51:34 +0530

    minor improvements made

--- app/components/aa-navbar.js
+++ app/components/aa-navbar.js
@@ -3,23 +3,11 @@ import { action } from "@ember/object";
 import { tracked } from '@glimmer/tracking';
 
 export default class aaNavBarComponent extends Component {
-    @tracked selectedTheme = false;
+    @tracked selectedTheme = (document.querySelector('html').classList.value === 'dark');
 
     toggleDarkTheme(match) {
         this.selectedTheme = match;
         document.querySelector('html').classList.toggle('dark', match);
-    }
-    constructor() {
-        super(...arguments);
-        if (typeof Window != "undefined") {
-            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && matchMedia('(prefers-color-scheme: dark)').matches)) {
-                localStorage.theme = 'dark';
-                this.toggleDarkTheme(true)
-            } else {
-                localStorage.theme = 'light';
-                this.toggleDarkTheme(false)
-            }
-        }
     }
     @action
     onToggle() {
